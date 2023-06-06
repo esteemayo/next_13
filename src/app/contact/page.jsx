d26ba05bcc03500d4e6164d4a990301d12161aa1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-06 07:54:25 +0100

    fix: add page' markup

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -4,7 +4,30 @@ import Button from '@/components/button/Button';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Let&apos;s Keep in Touch</h1>
+      <div className={styles.content}>
+        <div className={styles.imgContainer}>
+          <Image
+            src='/contact.png'
+            alt=''
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+        <form className={styles.form}>
+          <input type='text' placeholder='name' className={styles.input} />
+          <input type='text' placeholder='email' className={styles.input} />
+          <textarea
+            className={styles.textArea}
+            placeholder='message'
+            cols='30'
+            rows='10'
+          ></textarea>
+          <Button url='#' text='send' />
+        </form>
+      </div>
+    </div>
   );
 }
 
