302e1365255194ffcf429b6685eea59a22e06068
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-06 06:37:49 +0100

    fix: add classNames

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,12 +7,12 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <div className={styles.item}>
-        <h1>Better design for your digital products.</h1>
-        <p>
+        <h1 className={styles.title}>Better design for your digital products.</h1>
+        <p className={styles.desc}>
           Turning your Idea into Reality. We bring together the teams from the
           global tech industry.
         </p>
-        <button type='button'>see our works</button>
+        <button type='button' className={styles.button}>see our works</button>
       </div>
       <div className={styles.item}>
         <Image src={Hero} alt='' className={styles.img} />
