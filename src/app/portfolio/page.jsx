78e60f2122fa184b91a880989d2f3f0b197a6abd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-06 14:40:45 +0100

    fix: add page' markup

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -5,7 +5,18 @@ import styles from './page.module.scss';
 const Portfolio = () => {
   return (
     <div className={styles.container}>
-
+      <h1 className={styles.selectTitle}>Choose a gallery</h1>
+      <div className={styles.items}>
+        <Link href="/portfolio/illustrations" className={styles.item}>
+          <span className={styles.title}>Illustrations</span>
+        </Link>
+        <Link href="/portfolio/websites" className={styles.item}>
+          <span className={styles.title}>Websites</span>
+        </Link>
+        <Link href="/portfolio/application" className={styles.item}>
+          <span className={styles.title}>Application</span>
+        </Link>
+      </div>
     </div>
   );
 }
