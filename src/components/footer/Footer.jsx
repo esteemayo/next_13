442b22da545d5291de5cd7154d51b4ec2eb35f56
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-06 06:20:26 +0100

    fix: add className to image

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
     <footer className={styles.footer}>
       <div>&copy;2023 Esteem. All rights reserved.</div>
       <div className={styles.social}>
-        <Image src='/1.png' width={15} height={15} alt='' />
-        <Image src='/2.png' width={15} height={15} alt='' />
-        <Image src='/3.png' width={15} height={15} alt='' />
-        <Image src='/4.png' width={15} height={15} alt='' />
+        <Image src='/1.png' width={15} height={15} className={styles.icon} alt='' />
+        <Image src='/2.png' width={15} height={15} className={styles.icon} alt='' />
+        <Image src='/3.png' width={15} height={15} className={styles.icon} alt='' />
+        <Image src='/4.png' width={15} height={15} className={styles.icon} alt='' />
       </div>
     </footer>
   );
