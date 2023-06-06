1aa7ff0907f0ff184996f4cf4c8786df1eaed97f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-06 03:54:12 +0100

    fix: modify

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,4 @@
-import { Inter } from 'next/font/google'
+import { Inter } from 'next/font/google';
 
 import Navbar from '@/components/navbar/Navbar';
 import Footer from '@/components/footer/Footer';
