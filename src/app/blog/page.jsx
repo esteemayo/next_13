4159b7b9e66426742a4cc228a2682b241e918b2c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-06 17:23:03 +0100

    fix: add the page' markup

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -5,11 +5,11 @@ import styles from './page.module.scss';
 
 const Blog = () => {
   return (
-    <div className={styles.mainContainer}>
-      <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
+    <div className={styles.container}>
+      <Link href='#' className={styles.container}>
         <div className={styles.imageContainer}>
           <Image
-            src=''
+            src='https://images.pexels.com/photos/12225910/pexels-photo-12225910.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
             alt=""
             width={400}
             height={250}
@@ -17,8 +17,38 @@ const Blog = () => {
           />
         </div>
         <div className={styles.content}>
-          <h1 className={styles.title}>{item.title}</h1>
-          <p className={styles.desc}>{item.desc}</p>
+          <h1 className={styles.title}>test</h1>
+          <p className={styles.desc}>description</p>
+        </div>
+      </Link>
+      <Link href='#' className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src='https://images.pexels.com/photos/12225910/pexels-photo-12225910.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>test</h1>
+          <p className={styles.desc}>description</p>
+        </div>
+      </Link>
+      <Link href='#' className={styles.container}>
+        <div className={styles.imageContainer}>
+          <Image
+            src='https://images.pexels.com/photos/12225910/pexels-photo-12225910.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>test</h1>
+          <p className={styles.desc}>description</p>
         </div>
       </Link>
     </div>
