69bebe2aecd9cd76b2e807d3fcc0a88292ea30c5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-07 00:37:29 +0100

    fix: modify

--- src/components/darkModeToggle/DarkModeToggle.jsx
+++ src/components/darkModeToggle/DarkModeToggle.jsx
@@ -1,11 +1,13 @@
+'use client';
+
 import styles from './DarkModeToggle.module.scss';
 import { useThemeGlobalContext } from '@/context/ThemeContext';
 
 const DarkModeToggle = () => {
-  const { mode } = useThemeGlobalContext();
+  const { mode, toggle } = useThemeGlobalContext();
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={() => toggle()}>
       <div className={styles.icon}>🌙</div>
       <div className={styles.icon}>🔆</div>
       <div className={styles.ball} style={mode === 'light' ? { left: '2px' } : { right: '2px' }} />
