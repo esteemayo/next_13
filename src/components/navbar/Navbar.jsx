29f46d691e129b66e01d1461453b22579ec302a4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-06 04:16:53 +0100

    fix: add classnames

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,12 +6,12 @@ import styles from './Navbar.module.scss';
 const Navbar = () => {
   return (
     <nav className={styles.container}>
-      <Link href='/' passHref>Esteem</Link>
-      <div>
+      <Link href='/' passHref className={styles.logo}>Esteem</Link>
+      <div className={styles.links}>
         {links.map((link) => {
           const { id, url, title } = link;
           return (
-            <Link key={id} href={url} passHref>
+            <Link key={id} href={url} passHref className={styles.link}>
               {title}
             </Link>
           );
