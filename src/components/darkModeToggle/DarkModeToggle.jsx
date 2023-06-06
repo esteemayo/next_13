b4324e616db25b725a94915e23ccafd29351cd80
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-06 18:07:15 +0100

    feat: add DarkModeToggle component

---
+++ src/components/darkModeToggle/DarkModeToggle.jsx
@@ -0,0 +1,11 @@
+import styles from './DarkModeToggle.scss';
+
+const DarkModeToggle = () => {
+  return (
+    <div className={styles.container}>
+      DarkModeToggle
+    </div>
+  );
+}
+
+export default DarkModeToggle;
