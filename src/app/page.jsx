d3ef9001a1afe1afe686f57ea77fc913dda3f2c0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-06 06:36:24 +0100

    fix: add page' markup

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,19 @@ import styles from './page.module.scss';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1>Better design for your digital products.</h1>
+        <p>
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <button type='button'>see our works</button>
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt='' className={styles.img} />
+      </div>
+    </div>
   );
 }
 
