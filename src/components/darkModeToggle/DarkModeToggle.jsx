80b2252c2316e5a3e1e8aa409fc9c0f9cf7f8899
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-06 19:21:06 +0100

    fix: modify

--- src/components/darkModeToggle/DarkModeToggle.jsx
+++ src/components/darkModeToggle/DarkModeToggle.jsx
@@ -1,11 +1,13 @@
 import styles from './DarkModeToggle.module.scss';
 
 const DarkModeToggle = () => {
+  const mode = 'dark';
+
   return (
     <div className={styles.container}>
       <div className={styles.icon}>🌙</div>
       <div className={styles.icon}>🔆</div>
-      <div className={styles.ball} />
+      <div className={styles.ball} style={mode === 'light' ? { left: '2px' } : { right: '2px' }} />
     </div>
   );
 }
