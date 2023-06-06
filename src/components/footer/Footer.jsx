04d859eb866ae87e38f0b2ce8f59abf89b53079b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-06 06:22:32 +0100

    fix: add dynamic year to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,9 +2,12 @@ import Image from 'next/image';
 import styles from './Footer.module.scss';
 
 const Footer = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+
   return (
     <footer className={styles.footer}>
-      <div>&copy;2023 Esteem. All rights reserved.</div>
+      <div>&copy;{year} Esteem. All rights reserved.</div>
       <div className={styles.social}>
         <Image src='/1.png' width={15} height={15} className={styles.icon} alt='' />
         <Image src='/2.png' width={15} height={15} className={styles.icon} alt='' />
