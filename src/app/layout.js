52d15702cb5f4aa2bcff685c844db080bca28cfa
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-06 03:53:57 +0100

    fix: modify

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 
 import Navbar from '@/components/navbar/Navbar';
 import Footer from '@/components/footer/Footer';
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
