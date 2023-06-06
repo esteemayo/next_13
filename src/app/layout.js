959e5773dbc458268ff8279423888586afe6680b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-06 03:56:42 +0100

    fix: change fonts to Roboto

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={roboto.className}>
         <Navbar />
         {children}
         <Footer />
