85173384a3e0de678a4754fdd2825627c22d3dd5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-06 18:35:36 +0100

    fix: modify

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 
 import { links } from '@/data';
 import DarkModeToggle from '../darkModeToggle/DarkModeToggle';
+import styles from './Navbar.module.scss';
 
 const Navbar = () => {
   return (
