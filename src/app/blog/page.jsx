fa0c86e75c632230dc2d4980946fdf43637b52ed
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-06 17:20:11 +0100

    fix: modify

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,9 +1,27 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
+import styles from './page.module.scss';
+
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className={styles.mainContainer}>
+      <Link href={`/blog/${item._id}`} className={styles.container} key={item.id}>
+        <div className={styles.imageContainer}>
+          <Image
+            src=''
+            alt=""
+            width={400}
+            height={250}
+            className={styles.image}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>{item.title}</h1>
+          <p className={styles.desc}>{item.desc}</p>
+        </div>
+      </Link>
+    </div>
   );
 }
 
