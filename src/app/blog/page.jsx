1155efd980e204cd879a050333e8403ccad12cf9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-07 01:03:47 +0100

    fix: modify

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -5,7 +5,7 @@ import styles from './page.module.scss';
 
 async function getData() {
   const res = await fetch('http://jsonplaceholder.typicode.com/posts', {
-    cache: 'force-cache',
+    cache: 'no-store',
   });
 
   if (!res.ok) {
@@ -21,51 +21,26 @@ const Blog = async () => {
 
   return (
     <div className={styles.mainContainer}>
-      <Link href='/blog/testId' className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            src='https://images.pexels.com/photos/12225910/pexels-photo-12225910.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
-            alt=""
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>test</h1>
-          <p className={styles.desc}>description</p>
-        </div>
-      </Link>
-      <Link href='/blog/testId' className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            src='https://images.pexels.com/photos/12225910/pexels-photo-12225910.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
-            alt=""
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>test</h1>
-          <p className={styles.desc}>description</p>
-        </div>
-      </Link>
-      <Link href='/blog/testId' className={styles.container}>
-        <div className={styles.imageContainer}>
-          <Image
-            src='https://images.pexels.com/photos/12225910/pexels-photo-12225910.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
-            alt=""
-            width={400}
-            height={250}
-            className={styles.image}
-          />
-        </div>
-        <div className={styles.content}>
-          <h1 className={styles.title}>test</h1>
-          <p className={styles.desc}>description</p>
-        </div>
-      </Link>
+      {data.map((item) => {
+        const { id, body, title } = item;
+        return (
+          <Link key={id} href={`/blog/${encodeURIComponent(id)}`} className={styles.container}>
+            <div className={styles.imageContainer}>
+              <Image
+                src='https://images.pexels.com/photos/12225910/pexels-photo-12225910.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
+                alt=""
+                width={400}
+                height={250}
+                className={styles.image}
+              />
+            </div>
+            <div className={styles.content}>
+              <h1 className={styles.title}>{title}</h1>
+              <p className={styles.desc}>{body}</p>
+            </div>
+          </Link>
+        );
+      })}
     </div>
   );
 }
