4860268523572f080faede21c9ba936e06e92f82
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-07 15:40:11 +0100

    fix: fix a bug

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import styles from './page.module.scss';
 
 async function getData() {
-  const res = await fetch('http://localhost:300/api/posts', {
+  const res = await fetch('http://localhost:3000/api/posts', {
     cache: 'no-store',
   });
 
