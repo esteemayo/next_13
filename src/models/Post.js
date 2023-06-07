3d4b38c03effdcd549ec23061dab0f2f91c81294
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-07 16:07:57 +0100

    fix: add slug to the post schema

--- src/models/Post.js
+++ src/models/Post.js
@@ -30,6 +30,18 @@ const postSchema = new Schema({
   }
 );
 
+postSchema.pre('save', async function (next) {
+  if (!this.isModified('title')) return next();
+  this.slug = slugify(this.title, { lower: true });
+
+  const regEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const postWithSlug = await this.constructor.find({ slug: regEx });
+
+  if (postWithSlug.length) {
+    this.slug = `${this.slug}-${postWithSlug.length + 1}`;
+  }
+});
+
 const Post = mongoose.models.Post || mongoose.model('Post', postSchema);
 
 export default Post;
