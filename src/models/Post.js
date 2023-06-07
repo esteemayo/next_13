64c8b7a4616862ebb7c8eb2f2bb7bcf624a9fdc4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-07 14:50:01 +0100

    fix: create a post schema model

---
+++ src/models/Post.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const postSchema = new Schema({
+  title: {
+    type: String,
+    required: [true, 'A post must have a title'],
+  },
+  desc: {
+    type: String,
+    required: [true, 'A post must have a description'],
+  },
+  image: {
+    type: String,
+    required: [true, 'A post must have an image'],
+  },
+  content: {
+    type: String,
+    required: [true, 'A post must have a content'],
+  },
+  username: {
+    type: String,
+    required: [true, 'A post must belong to a user'],
+  },
+},
+  {
+    timestamps: true,
+  }
+);
+
+const Post = mongoose.models.Post || mongoose.model('Post', postSchema);
+
+export default Post;
