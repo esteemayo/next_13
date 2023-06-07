be826ac6d56c2e18b6077d6243ff99f615439000
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-07 01:27:36 +0100

    fix: fetch data

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -4,6 +4,22 @@ import { useEffect, useState } from 'react';
 
 const Dashboard = () => {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch('http://jsonplaceholder.typicode.com/posts');
+
+      if (!res.ok) {
+        setIsError(true);
+      }
+
+      const data = await res.json();
+      setData(data);
+      setIsLoading(false);
+    })();
+  }, []);
 
   return (
     <div>Dashboard</div>
