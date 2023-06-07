41f5bdd12e1be05c42cf3922442a1b9372b1f7b8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-07 01:39:17 +0100

    fix: modify

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -7,19 +7,19 @@ const Dashboard = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
 
-  useEffect(() => {
-    (async () => {
-      const res = await fetch('http://jsonplaceholder.typicode.com/posts');
+  // useEffect(() => {
+  //   (async () => {
+  //     const res = await fetch('http://jsonplaceholder.typicode.com/posts');
 
-      if (!res.ok) {
-        setIsError(true);
-      }
+  //     if (!res.ok) {
+  //       setIsError(true);
+  //     }
 
-      const data = await res.json();
-      setData(data);
-      setIsLoading(false);
-    })();
-  }, []);
+  //     const data = await res.json();
+  //     setData(data);
+  //     setIsLoading(false);
+  //   })();
+  // }, []);
 
   return (
     <div>Dashboard</div>
