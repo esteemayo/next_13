13d8e3a7906976510860db0e72290978854fcf77
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-08 00:56:15 +0100

    fix: handle fetch post by slug functionality

--- src/app/api/posts/[slug]/route.js
+++ src/app/api/posts/[slug]/route.js
@@ -1,2 +1,21 @@
 import { NextResponse } from 'next/server';
 import Post from '@/models/Post';
+import connectDB from '@/utils/db';
+
+export const GET = async (request, { params }) => {
+  const { slug } = params;
+
+  try {
+    await connectDB();
+
+    const post = await Post.findOne({ slug });
+
+    return NextResponse.json(post, {
+      status: 200,
+    });
+  } catch (err) {
+    return NextResponse.json('Database Error', {
+      status: 500,
+    });
+  }
+}
