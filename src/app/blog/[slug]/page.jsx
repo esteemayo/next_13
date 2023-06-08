951ff7614ef4b2c91cfce127544a9b5f94410247
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-08 01:04:49 +0100

    fix: modify

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -24,9 +24,7 @@ const BlogPost = async ({ params: { slug } }) => {
       <div className={styles.top}>
         <div className={styles.info}>
           <h1 className={styles.title}>{data.title}</h1>
-          <p className={styles.desc}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione ipsa debitis voluptatibus iure earum explicabo, facilis laboriosam eligendi repudiandae, temporibus distinctio delectus expedita dolore beatae dolorem accusamus et aliquid architecto?
-          </p>
+          <p className={styles.desc}>{data.desc}</p>
           <div className={styles.author}>
             <Image
               src='https://images.pexels.com/photos/12225910/pexels-photo-12225910.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load'
