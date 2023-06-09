de84f9cae6aacf48afc8f1024cbc464f20b8f6b4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-09 18:56:44 +0100

    fix: handle register user functionality

---
+++ src/app/api/auth/register/route.js
@@ -0,0 +1,31 @@
+import { NextResponse } from 'next/server';
+import connectDB from '@/utils/db';
+import User from '@/models/User';
+
+export const POST = async (request) => {
+  const { name, email, username, password, confirmPassword } = await request.json();
+
+  await connectDB();
+
+  const newUser = {
+    name,
+    email,
+    username,
+    password,
+    confirmPassword,
+  };
+
+  try {
+    const user = await User.create({ ...newUser });
+
+    if (user) {
+      return NextResponse.json(user, {
+        status: 201,
+      });
+    }
+  } catch (err) {
+    return NextResponse.json(err.message, {
+      status: 500,
+    });
+  }
+};
