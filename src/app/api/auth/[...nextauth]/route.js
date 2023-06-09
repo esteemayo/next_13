9359105a680bdd84b9cf0ab5c0796539d0201a3b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-09 20:49:52 +0100

    fix: modify

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from 'next/providers/credentials';
+
 import User from '@/models/User';
 import connectDB from '@/utils/db';
 
