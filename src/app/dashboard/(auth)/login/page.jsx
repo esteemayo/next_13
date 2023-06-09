2618d614aaded952199806c79a814b6e53f9043c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-09 23:34:42 +0100

    fix: add page' markup

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -4,8 +4,28 @@ import { signIn } from 'next-auth/react';
 import styles from './page.module.scss';
 
 const Login = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className={styles.container}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input
+          type='email'
+          placeholder='Email'
+          required
+          className={styles.input}
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          required
+          className={styles.input}
+        />
+        <button type='submit' className={styles.button}>Login</button>
+        {error && 'Something went wrong!'}
+      </form>
       <button type='button' onClick={() => signIn('google')}>
         Login with Google
       </button>
