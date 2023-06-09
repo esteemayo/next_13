37d2418b5441b1acc86e6152b6c6ba1324c74d68
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-09 02:13:48 +0100

    fix: modify

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,6 +28,7 @@ export default function RootLayout({ children }) {
             {children}
             <Footer />
           </div>
+          </AuthProvider>
         </ThemeProvider>
       </body>
     </html>
