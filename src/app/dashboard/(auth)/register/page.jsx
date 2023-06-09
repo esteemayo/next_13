19fcb0e927dfe9fe4e5e6a3723d5db9ec90a49f5
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-09 04:31:57 +0100

    fix: add page' markup

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -5,8 +5,43 @@ import { useState } from 'react';
 import styles from './page.module.scss';
 
 const Register = () => {
+  const [error, setError] = useState(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>Register</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Create an Account</h1>
+      <h2 className={styles.subtitle}>Please sign up to see the dashboard.</h2>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input
+          type='text'
+          placeholder='Username'
+          required
+          className={styles.input}
+        />
+        <input
+          type='text'
+          placeholder='Email'
+          required
+          className={styles.input}
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          required
+          className={styles.input}
+        />
+        <button className={styles.button}>Register</button>
+        {error && 'Something went wrong!'}
+      </form>
+      <span className={styles.or}>- OR -</span>
+      <Link className={styles.link} href='/dashboard/login'>
+        Login with an existing account
+      </Link>
+    </div>
   );
 }
 
