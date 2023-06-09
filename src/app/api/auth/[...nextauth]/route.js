32171c46b14111d779f07b5dcbd33fea6da75ec2
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-09 20:36:21 +0100

    feat: add CredentialsProvider

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next/providers/credentials';
 
 const handler = NextAuth({
   providers: [
