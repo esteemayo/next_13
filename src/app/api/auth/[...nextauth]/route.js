4cd8c055ea96ff7c9532b9085c5ab758cb1c583b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-10 01:19:23 +0100

    feat: add bcryptjs module

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,4 +1,5 @@
 import NextAuth from 'next-auth';
+import bcrypt from 'bcryptjs';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GoogleProvider from 'next-auth/providers/google';
 
