06cfed6c705b8dd017b2468cd18e64dae157ddaa
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-10 03:09:48 +0100

    fix: add page' markup

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -18,6 +18,10 @@ const Dashboard = () => {
     fetcher,
   );
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   if (session.status === 'loading') {
     return <p>Loading...</p>;
   }
@@ -28,7 +32,34 @@ const Dashboard = () => {
 
   if (session.status === 'authenticated') {
     return (
-      <div className={styles.container}>Dashboard</div>
+      <div className={styles.container}>
+        <div className={styles.posts}>
+          <div className={styles.post}>
+            <div className={styles.imgContainer}>
+              <Image src='https://images.pexels.com/photos/12225910/pexels-photo-12225910.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load' alt='' width={200} height={100} />
+            </div>
+            <h2 className={styles.postTitle}>My first blog post</h2>
+            <span
+              className={styles.delete}
+            >
+              X
+            </span>
+          </div>
+        </div>
+        <form className={styles.new} onSubmit={handleSubmit}>
+          <h1>Add New Post</h1>
+          <input type='text' placeholder='Title' className={styles.input} />
+          <input type='text' placeholder='Desc' className={styles.input} />
+          <input type='text' placeholder='Image' className={styles.input} />
+          <textarea
+            placeholder='Content'
+            className={styles.textArea}
+            cols='30'
+            rows='10'
+          ></textarea>
+          <button className={styles.button}>Send</button>
+        </form>
+      </div>
     );
   }
 }
