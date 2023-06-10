e20024e6fcd689db0b129207ae583df76c982aa3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-10 17:40:47 +0100

    fix: add useSearchParams  next/navigation hook

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import { signIn, useSession } from 'next-auth/react';
 import { useState } from 'react';
 import Link from 'next/link';
