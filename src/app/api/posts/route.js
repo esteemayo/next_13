f7bf99ae5a51f97be0560b58c6adb2c76c7e4c7c
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-10 03:02:57 +0100

    fix: handle query parameter

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -4,10 +4,13 @@ import Post from '@/models/Post';
 import connectDB from '@/utils/db';
 
 export const GET = async (request) => {
+  const { searchParams } = new URL(request.url);
+  const username = searchParams.get('username');
+
   try {
     await connectDB();
 
-    const posts = await Post.find();
+    const posts = await Post.find(username && { username });
 
     return NextResponse.json(posts, {
       status: 200,
