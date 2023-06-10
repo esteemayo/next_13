e9d1f2afd5b8f9e14461d1ef8e15ec97ac82d491
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-10 02:17:29 +0100

    fix: modify

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -9,7 +9,6 @@ import { useRouter } from 'next/navigation';
 const Login = () => {
   const router = useRouter();
   const session = useSession();
-  const [error, setError] = useState();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
