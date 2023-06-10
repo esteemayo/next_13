4d4a6ef01ec422a4e8dd6e1889d4877392d86992
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-10 02:21:09 +0100

    fix: modify

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -9,23 +9,6 @@ import styles from './page.module.scss';
 const Dashboard = () => {
   const router = useRouter();
   const session = useSession();
-  // const [data, setData] = useState([]);
-  // const [isLoading, setIsLoading] = useState(true);
-  // const [isError, setIsError] = useState(false);
-
-  // useEffect(() => {
-  //   (async () => {
-  //     const res = await fetch('http://jsonplaceholder.typicode.com/posts');
-
-  //     if (!res.ok) {
-  //       setIsError(true);
-  //     }
-
-  //     const data = await res.json();
-  //     setData(data);
-  //     setIsLoading(false);
-  //   })();
-  // }, []);
 
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
