9e222e8d5119f4db56f7ed49a3cd519e2a273ccd
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-06-10 01:52:36 +0100

    fix: modify

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,6 @@
 'use client';
 
 import useSWR from 'swr';
-import { useEffect, useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
